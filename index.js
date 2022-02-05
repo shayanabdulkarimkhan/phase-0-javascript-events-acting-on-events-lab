8594d4d641a8071f34371c5971a41aff03fddafe
shayan <sak704@nyu.edu>
2022-02-05 15:53:42 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    const rightEdge = left + 40;
+
+    const right = 400 - rightEdge;
+
+    //const widthNumbers = dodger.style.width.replace("px","");
+    //const rightEdge = left + parseInt(widthNumbers,10);
+
+    //const gameWidth = game.style.width.replace("px","");
+
+    //const right = parseInt(gameWidth) - rightEdge;
+
+    if (rightEdge < 400) {
+        dodger.style.left = `${left+1}px`;
+    }
+};
+
+document.addEventListener("keydown",function (e) {
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
